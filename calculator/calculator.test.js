6b2b882e0b2679196deea3006134211dbd1930f8
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-04 10:48:42 +0100

    Added the calculator task

---
+++ calculator/calculator.test.js
@@ -0,0 +1,23 @@
+const calculator = require('./calculator.js');
+
+describe('calculator', () => { 
+  test('Addition', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+  });
+
+  test('Subtraction', () => {
+    expect(calculator.subtract(7, 3)).toBe(4);
+  });
+
+  test('Multiply', () => {
+    expect(calculator.multiply(8, 2)).toBe(16);
+  });
+
+  test('Division', () => {
+    expect(calculator.divide(12, 4)).toBe(3);
+  })
+/*
+  test('Division', () => {
+    expect(calculator.divide(0, 4)).toThrow('Mathematical Error');
+  }) */
+});
